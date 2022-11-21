29d7e4f6817b89dc68e2c762cb67336ab6220809
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-21 09:45:51 +0100

    task_5 changes

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,10 +8,9 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node)
-	{
-		if (node->left != NULL && node->right != NULL && node->parent == NULL)
-			return (1);
-	}
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
 	return (0);
 }
