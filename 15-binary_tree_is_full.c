da3f7ac18e4749b6dfa988fa84426260ba6696cf
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-21 10:35:10 +0100

    task_15 debug

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,70 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * power - function
- * @a: member
- * Return: power of 2 to a
- */
-
-int power(int a)
-{
-	int i = 0, res = 1;
-
-	while (i < a)
-	{
-		res *= 2;
-		i++;
-	}
-
-
-	return (res);
-}
-
-
-
-/**
- * height - function
- * @tree: node to check
- * Return: 1 for Success
- */
-
-int height(const binary_tree_t *tree)
-{
-	int r, l;
-
-	if (!tree)
-		return (0);
-
-	if (tree && (tree->left || tree->right))
-	{
-		l = 1 + height(tree->left);
-		r = 1 + height(tree->right);
-
-		return (l > r ? l : r);
-	}
-	return (0);
-}
-
-/**
- * size - function
- * @tree: node to check
- * Return: 1 for Success
- */
-
-int size(const binary_tree_t *tree)
-{
-	int s;
-
-	if (tree)
-	{
-
-		s = 1 + size(tree->left) + size(tree->right);
-
-		return (s);
-	}
-	return (0);
-}
-/**
  * binary_tree_is_full - function
  * @tree: node to check
  * Return: integer
@@ -72,14 +8,14 @@ int size(const binary_tree_t *tree)
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int h, s;
 
 	if (tree)
 	{
-		h = height(tree);
-		s = size(tree);
-		if (power(h + 1) - 1 == s)
+		if (tree->right == NULL && tree->left == NULL)
 			return (1);
+		if (tree->right && tree->left)
+			return (binary_tree_is_full(tree->left) &&
+					binary_tree_is_full(tree->right));
 	}
 
 
