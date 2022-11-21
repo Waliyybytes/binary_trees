e58e223596f026ce28a6e79db931933f7ec5bd69
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-21 10:20:13 +0100

    task_12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,11 +11,11 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		return (1 + (binary_tree_leaves(tree->left) != 0 &&
-					binary_tree_leaves(tree->right) != 0));
+		if (tree->left == NULL && tree->right == NULL)
+			return (1);
+		return (binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
 	}
 
 
 	return (0);
 }
-
